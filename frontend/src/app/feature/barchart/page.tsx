6e0f320eef76c8c7794d25ce8b73de74946674c3
smileycookie <ripunjaysinghlikun@gmail.com>
2025-06-14 20:37:03 +0530

    Added the Barchart feature.

---
+++ frontend/src/app/feature/barchart/page.tsx
@@ -0,0 +1,13 @@
+
+// src/app/bargraph/page.tsx
+"use client"
+
+import { ChartBarInteractive } from "@/components/Bargraph"
+
+export default function Page() {
+  return (
+    <main className="p-6">
+      <ChartBarInteractive />
+    </main>
+  )
+}
